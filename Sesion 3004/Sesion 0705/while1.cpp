12b8140b78cb33bccc959cbfb261a69a8d6c5a07
fmariezgg <fzogaib@uamv.edu.ni>
2024-05-07 20:20:21 -0600

    sesion 0705

---
+++ Sesion 3004/Sesion 0705/while1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//Se usa for cuando conozco el punto de algo
+//For y While son parecidos
+//No es finito
+int main()   
+{
+    while (true)
+        {
+         cout << "01";
+            false;
+        }
+    
+    return 0;
+}
+
---
+++ Sesion 3004/Sesion 0705/while3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// No es finito
+// Se usa cuando no conozco cuantas veces voy a repetir el ciclo
+int main()
+{
+    int op;
+    while (op != -1)
+    {
+        cout << "Dime un numero: ";
+        cin >> op;
+    }
+}
