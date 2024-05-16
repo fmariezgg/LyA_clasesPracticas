b234b3e0ebaa8d971fa9cdfea01701ab4fe8b06f
fmariezgg <fzogaib@uamv.edu.ni>
2024-05-15 19:25:35 -0600

    seseion 1405

---
+++ Sesion 1405/funciones.cpp
@@ -0,0 +1,30 @@
+/*Funciones*/
+
+#include <stdio.h>
+#include <
+#include <stdlib.h>
+
+/*Saludar*/
+
+void showMessage();
+void showMessage()
+{
+
+    printf("Universidad Americana\n");
+    printf("UAM\n");
+}
+
+int suma();
+int suma()
+{
+    int suma, a, b;
+    int a = 0;
+    int b = 0;
+    suma = a + b;
+    printf(" Ingresa el primer numero\n");
+    scanf("%i", a);
+    printf(" Ingresa el segundo numero\n");
+    scanf("%i", b);
+    return suma;
+
+}
---
+++ Sesion 1405/principal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "funciones.cpp"
+
+using namespace std;
+
+int main(){
+    showMessage();
+    suma();
+    
+
+    return 0;
+
+}
+
